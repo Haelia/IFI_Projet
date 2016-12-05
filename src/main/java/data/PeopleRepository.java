184916d20c8e89f4431c7bbd4e652b8bdbb5be3b
Narxem <adrien.agez@hotmail.fr>
2016-12-05 17:11:50 +0100

    update pom.xml + create 1st controller

--- src/main/java/data/PeopleRepository.java
+++ src/main/java/data/PeopleRepository.java
@@ -4,7 +4,21 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface PeopleRepository extends CrudRepository<People, Long> {
+public interface PeopleRepository extends CrudRepository<People, Integer> {
+
+    People save(People people);
 
     List<People> findAll();
+
+    People findByFirstNameAndLastName(String firstName, String lastName);
+
+    People findByPhone(String phone);
+
+    People findByEmail(String email);
+
+    long count();
+
+    void delete(People people);
+
+    boolean exists(Long id);
 }
